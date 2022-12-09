24ca2a7dad239aa13020b784d5de9148ea2bf9d1
MaxKorovchenko <max.korovchenko@gmail.com>
2022-12-09 16:38:50 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+button.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+  span.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+}
