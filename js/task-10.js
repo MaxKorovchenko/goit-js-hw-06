4e0a88c175f12c26ff2ca179c8b38e9b5cb3cf68
MaxKorovchenko <max.korovchenko@gmail.com>
2022-12-09 21:22:02 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,41 @@
+// IN PROGRESS//
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-in progress...😃
+const boxes = document.querySelector("#boxes");
+const input = document.querySelector("input");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+
+let amount = 10;
+
+input.addEventListener("input", onInputAction);
+btnCreate.addEventListener("click", onClickCreate);
+btnDestroy.addEventListener("click", onClickDestroy);
+
+function onInputAction(event) {
+  amount = Number(event.currentTarget.value);
+  console.log(amount);
+}
+
+function createBoxes(amount) {
+  console.log(amount);
+
+  for (let i = 1; i <= amount; i += 1) {
+    const div = document.createElement("div");
+    div.style.width = `${i * 10 + 20}px`;
+    div.style.height = `${i * 10 + 20}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    boxes.append(div);
+  }
+}
+
+function onClickCreate() {
+  createBoxes(amount);
+}
+
+function onClickDestroy() {
+  boxes.innerHTML = "";
+}
