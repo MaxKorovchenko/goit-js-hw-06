27ae3f1e83396e336637a54195b0e209a20f8ad4
MaxKorovchenko <max.korovchenko@gmail.com>
2022-12-09 16:12:11 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value === "" || password.value === "") {
+    return alert("You need to fill all the required fields");
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
