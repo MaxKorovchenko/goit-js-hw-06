63d45a8f76fc95b33f453d5c17a7f040fd0ee283
MaxKorovchenko <max.korovchenko@gmail.com>
2022-12-09 14:47:41 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const input = document.querySelector("#validation-input");
+console.log(input);
+
+input.addEventListener("blur", onInputBlur);
+
+function onInputBlur(event) {
+  const target = event.currentTarget;
+  if (target.value.length === Number(target.dataset.length)) {
+    target.classList.add("valid");
+    target.classList.remove("invalid");
+  } else {
+    target.classList.add("invalid");
+    target.classList.remove("valid");
+  }
+}
